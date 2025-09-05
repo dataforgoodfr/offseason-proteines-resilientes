dataforgoodfr/offseason-proteines-resilientes

commit 41750d3ad6f54b9497e5fb9dfcce91f0dcdc6b2c

Add '--force' CLI option

src/off_data_collector/cli.py
@@ -4,7 +4,8 @@ from logging import DEBUG, INFO
 from pathlib import Path
 
 from openfoodfacts import API as off_api
-from sqlalchemy import create_engine, func
+from openfoodfacts.types import JSONType
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
 from .api import FIELDS
@@ -38,7 +39,7 @@ def __get_arg_parser() -> ArgumentParser:
 
     ref_group = arg_parser.add_mutually_exclusive_group()
 
-    ref_group.add_argument("references", nargs="*", help="Product references")
+    ref_group.add_argument("references", nargs="*", help="Product references (EAN-13)")
     ref_group.add_argument(
         "--ref-file",
         "-f",
@@ -50,6 +51,13 @@ def __get_arg_parser() -> ArgumentParser:
         "--database",
         default=DEFAULT_DATABASE_URL,
         help=f"Database URL to use (defaults to {DEFAULT_DATABASE_URL})",
+    )
+
+    arg_parser.add_argument(
+        "--force",
+        action="store_true",
+        default=False,
+        help="Product data will be updated even if the product already exists",
     )
 
     arg_parser.add_argument(
@@ -124,80 +132,118 @@ def main() -> None:
         if reference == "" or reference.startswith("#"):
             continue
 
-        logger.debug(f"Processing reference '{reference}'")
+        logger.debug(f"Processing reference ID '{reference}'")
 
         with Session(engine) as session:
-            ref_count = (
-                session.query(func.count())
-                .select_from(Product)
-                .filter(Product.ean_13 == reference)
-                .scalar()
+            products = session.scalars(
+                select(Product).where(Product.ean_13 == reference)
             )
 
-        if ref_count != 0:
-            logger.debug(f"Reference ID {reference} already in database")
-            continue
+            # If the product does not already exist in the database.
+            if products == 0:
+                data = __fetch_product_data(api_client, reference)
+                new_product = __create_new_product(reference, data)
 
-        data = api_client.product.get(
-            reference,
-            fields=FIELDS,
-        )
+                session.add(new_product)
+                session.commit()
+            else:
+                logger.debug(f"Reference ID {reference} already in database")
 
-        # If the product was not found.
-        if data is None or data.get("product_name") is None:
-            logger.debug(f"Reference not found: '{reference}'")
-            continue
+                # If the user has not explicitly asked to update the products that
+                # are already present in the database.
+                if not args.force:
+                    logger.debug(f"Skipping product with reference ID '{reference}'")
+                    continue
 
-        logger.debug(f"Data received: {data}")
+                product = products.one()
 
-        # TODO: Refactor the Product instantiation.
-        new_product = Product(
-            ean_13=reference,
-            name=data["product_name"],
-            brand=data["brands"] if data.get("brands") else None,
-            sources=[
-                Source(
-                    url=f"https://world.openfoodfacts.org/product/{reference}/",
-                    nutrition_facts=NutritionFacts(
-                        nutriscore=NutriScore(data["nutrition_grade_fr"])
-                        if data.get("nutrition_grade_fr")
-                        and data["nutrition_grade_fr"] != "unknown"
-                        and data["nutrition_grade_fr"] != "not-applicable"
-                        else None,
-                        novascore=NovaScore(data["nova_group"])
-                        if data.get("nova_group")
-                        else None,
-                        calories_100g=float(data["energy-kcal_100g"])
-                        if data.get("energy-kcal_100g")
-                        else None,
-                        fat_100g=float(data["fat_100g"])
-                        if data.get("fat_100g")
-                        else None,
-                        saturated_fat_100g=float(data["saturated-fat_100g"])
-                        if data.get("saturated-fat_100g")
-                        else None,
-                        carbohydrates_100g=float(data["carbohydrates_100g"])
-                        if data.get("carbohydrates_100g")
-                        else None,
-                        sugars_100g=float(data["sugars_100g"])
-                        if data.get("sugars_100g")
-                        else None,
-                        fiber_100g=float(data["fiber_100g"])
-                        if data.get("fiber_100g")
-                        else None,
-                        protein_100g=float(data["proteins_100g"])
-                        if data.get("proteins_100g")
-                        else None,
-                        salt_100g=float(data["salt_100g"])
-                        if data.get("salt_100g")
-                        else None,
-                    ),
-                ),
-            ],
-        )
+                if product.disabled:
+                    logger.debug(f"Product with reference ID '{reference}' is disabled")
+                    continue
 
-        with Session(engine) as session:
-            session.add(new_product)
-            session.commit()
+                data = __fetch_product_data(api_client, reference)
+                __update_product(product, reference, data)
+
+                session.commit()
 
     logger.info("Program ended")
+
+
+def __fetch_product_data(
+    api_client: off_api, reference: str, data_fields: list[str] = FIELDS
+) -> JSONType | None:
+    """
+    Fetches the product data from OpenFoodFacts for a given reference (EAN-13).
+    """
+
+    logger = getLogger(__name__)
+
+    data = api_client.product.get(
+        reference,
+        fields=data_fields,
+    )
+
+    # If the product was not found.
+    if data is None or data.get("product_name") is None:
+        logger.debug(f"Reference not found: '{reference}'")
+        return None
+
+    logger.debug(f"Data received: {data}")
+    return data
+
+
+def __create_new_product(reference: str, data: JSONType) -> Product:
+    """
+    Returns a new Product object based on the data fetched from OpenFoodFacts.
+    """
+
+    return Product(
+        ean_13=reference,
+        name=data["product_name"],
+        brand=data["brands"] if data.get("brands") else None,
+        sources=[__create_new_source(reference, data)],
+    )
+
+
+def __update_product(product: Product, reference: str, data: JSONType) -> None:
+    """
+    Updates a product stored in the database by adding the product data fetched
+    from OpenFoodFacts as new source.
+    """
+
+    product.sources.append(__create_new_source(reference, data))
+
+
+def __create_new_source(reference: str, data: JSONType) -> Source:
+    """
+    Returns a new Source object based on the reference ID and the data fetched
+    from OpenFoodFacts given as parameters.
+    """
+
+    return Source(
+        url=f"https://world.openfoodfacts.org/product/{reference}/",
+        nutrition_facts=NutritionFacts(
+            nutriscore=NutriScore(data["nutrition_grade_fr"])
+            if data.get("nutrition_grade_fr")
+            and data["nutrition_grade_fr"] != "unknown"
+            and data["nutrition_grade_fr"] != "not-applicable"
+            else None,
+            novascore=NovaScore(data["nova_group"]) if data.get("nova_group") else None,
+            calories_100g=float(data["energy-kcal_100g"])
+            if data.get("energy-kcal_100g")
+            else None,
+            fat_100g=float(data["fat_100g"]) if data.get("fat_100g") else None,
+            saturated_fat_100g=float(data["saturated-fat_100g"])
+            if data.get("saturated-fat_100g")
+            else None,
+            carbohydrates_100g=float(data["carbohydrates_100g"])
+            if data.get("carbohydrates_100g")
+            else None,
+            sugars_100g=float(data["sugars_100g"]) if data.get("sugars_100g") else None,
+            fiber_100g=float(data["fiber_100g"]) if data.get("fiber_100g") else None,
+            protein_100g=float(data["proteins_100g"])
+            if data.get("proteins_100g")
+            else None,
+            salt_100g=float(data["salt_100g"]) if data.get("salt_100g") else None,
+        ),
+    )

commit ee547fb91235e136dfb12d06e12ffb590a41f8d4

Fix OFFDC product counting logic
A bug was introduced in 41750d3a, breaking the product counting logic
and resulting in all references considered as being already present in
the database.

src/off_data_collector/cli.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from openfoodfacts import API as off_api
 from openfoodfacts.types import JSONType
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from .api import FIELDS
@@ -131,12 +131,10 @@ def main() -> None:
         logger.debug(f"Processing reference ID '{reference}'")
 
         with Session(engine) as session:
-            products = session.scalars(
-                select(Product).where(Product.ean_13 == reference)
-            )
+            products = session.query(Product).filter(Product.ean_13 == reference)
 
             # If the product does not already exist in the database.
-            if products == 0:
+            if products.count() == 0:
                 data = __fetch_product_data(api_client, reference)
                 new_product = __create_new_product(reference, data)
 