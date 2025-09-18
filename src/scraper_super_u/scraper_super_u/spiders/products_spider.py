055d65960c5cb2d09b31ade3bd11eefeea60d375
Dadoo <contact@david-libert.com>
2025-09-18 13:49:12 +0200

    reorganize scraper_superu folder as he was at the begining

--- src/scraper_super_u/scraper_super_u/spiders/products_spider.py
+++ src/scraper_super_u/scraper_super_u/spiders/products_spider.py
@@ -4,24 +4,7 @@ import scrapy
 class ProductsSpiderSpider(scrapy.Spider):
     name = "products_spider"
     allowed_domains = ["scrapingclub.com"]
-
-    def start_requests(self):
-        url = "https://scrapingclub.com/exercise/list_infinite_scroll/"
-        yield scrapy.Request(url, meta={"playwright": True})
+    start_urls = ["https://scrapingclub.com/exercise/list_infinite_scroll/"]
 
     def parse(self, response):
-        # iterate over the product elements
-        for product in response.css(".post"):
-            # scrape product data
-            url = product.css("a").attrib["href"]
-            image = product.css(".card-img-top").attrib["src"]
-            name = product.css("h4 a::text").get()
-            price = product.css("h5::text").get()
-
-            # add the data to the list of scraped items
-            yield {
-                "url": url,
-                "image": image,
-                "name": name,
-                "price": price
-            }
+        pass
