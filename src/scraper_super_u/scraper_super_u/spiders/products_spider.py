f31c7a1859c4c3311a14b51c60dc28378714cde7
Dadoo <contact@david-libert.com>
2025-09-18 13:51:22 +0200

    allocate real url to new spider

--- src/scraper_super_u/scraper_super_u/spiders/products_spider.py
+++ src/scraper_super_u/scraper_super_u/spiders/products_spider.py
@@ -3,8 +3,8 @@ import scrapy
 
 class ProductsSpiderSpider(scrapy.Spider):
     name = "products_spider"
-    allowed_domains = ["scrapingclub.com"]
-    start_urls = ["https://scrapingclub.com/exercise/list_infinite_scroll/"]
+    allowed_domains = ["coursesu.com"]
+    start_urls = ["https://www.coursesu.com/recherche?q=poulet"]
 
     def parse(self, response):
         pass
