230922625cf407fc97da95a066e0e2bf3888dded
Dadoo <contact@david-libert.com>
2025-09-19 13:25:54 +0200

    add nodriver to products-spider to scrap

--- src/scraper_superu/__scraper_superu.py
+++ src/scraper_superu/__scraper_superu.py
@@ -3,7 +3,6 @@ import os
 import json
 from bs4 import BeautifulSoup
 from typing import List, Dict
-from urllib.parse import quote_plus
 from logging import getLogger, StreamHandler
 from logging import INFO
 from abstract.isite_connector import ISiteConnector
@@ -20,44 +19,16 @@ class SuperuScraper:
         self.superu_logger.setLevel(INFO)
         self.superu_logger.addHandler(StreamHandler())
 
-    def get_pages(self, product: str, n_pages: int = 1) -> List[str]:
-        url: str = f"https://www.coursesu.com/recherche?q={quote_plus(product).replace(' ', '+')}"
+    def get_pages(self, url: str, n_pages: int = 1) -> List[str]:
         if n_pages > 1:
             return [
-                self.connector.get_page(f"{url}&page={page_number + 1}")
+                self.connector.get_page(f"{url}?page={page_number + 1}")
                 for page_number in range(n_pages)
             ]
         else:
             return [self.connector.get_page(url)]
 
-    def parse_data(self, html: str) -> List[Dict]:
-        try:
-            soup: BeautifulSoup = BeautifulSoup(html, "html.parser")
-            products: List[Dict] = []
-
-            # extraction of json data from data-tc-product-tile attribute
-            tiles: List[BeautifulSoup] = soup.select("li[data-tc-product-tile]")
-            for li in tiles:
-                raw_data: str = li.get("data-tc-product-tile")
-                if raw_data:
-                    json_str: str = raw_data.replace("&quot;", '"')
-                    data: Dict = json.loads(json_str)
-                    products.append({
-                        "ean": data.get("EAN"),
-                        "name": data.get("name"),
-                        "price": data.get("price"),
-                        "brand": data.get("brand"),
-                        "category1": data.get("product_cat1"),
-                        "category2": data.get("product_cat2"),
-                        "category3": data.get("product_cat3"),
-                        "image": data.get("product_url_picture"),
-                    })
-            return products
-        except json.JSONDecodeError:
-            self.superu_logger.error("Erreur de parsing JSON:", raw_data)
-        except Exception as e:
-            self.superu_logger.error(f"Error parsing HTML: {e}")
-            return []
+    
 
     
         
--- src/scraper_superu/products_spider.py
+++ src/scraper_superu/products_spider.py
@@ -2,11 +2,13 @@ import asyncio
 import scrapy
 from scrapy import Request, Spider
 from urllib.parse import quote_plus
-from typing import Dict
+from typing import Dict, List
+from bs4 import BeautifulSoup
+import json
 
 from .items import ProductItem
-# from playwright.async_api import async_playwright
 from .connector_nodriver import ConnectorNodriver
+
 
 # Name of the cookie used to specify the "journey" ID.
 #
@@ -15,7 +17,7 @@ from .connector_nodriver import ConnectorNodriver
 #
 # See https://github.com/dataforgoodfr/offseason-proteines-resilientes/issues/2
 # for more information.
-JOURNEY_COOKIE_NAME = "storeId"
+
 
 
 class SuperUProductsSpider:
@@ -27,260 +29,7 @@ class SuperUProductsSpider:
     allowed_domains = ["www.coursesu.com"]
     start_urls = ["data:,"]
     custom_settings = {}
-    cookies=[
-                {
-                    "domain": "www.coursesu.com",
-                    "expirationDate": 1760788969.193743,
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "newsletterDisplayNb",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "0"
-                },
-                {
-                    "domain": ".coursesu.com",
-                    "expirationDate": 1758198770.193917,
-                    "hostOnly": "false",
-                    "httpOnly": "true",
-                    "name": "__cf_bm",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "UOb4zV2b4AY9.joEXJ61.iMJz5pcz1ifWfHPmHIF2RU-1758196970-1.0.1.1-MDKt0p5ZQEEGbdTFFC5z5MQ3EC0pdqzGFomK3_eG3UgFwAsM6ifT2YufcB423Ap0pCaCkAscysXcszCtHHuEViBpQVAcdo_5W.20HmWx4Ck"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "hostOnly": "true",
-                    "httpOnly": "true",
-                    "name": "dwsid",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "true",
-                    "storeId": "null",
-                    "value": "eAmxjxSFBB7r8RALohtJ7zQL9tRurCAvYJOrtOg-nBOYvaodj-ADE0fvqbMtI03nXOm-1lCJu04k0DdlXMACPQ=="
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "cquid",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "true",
-                    "storeId": "null",
-                    "value": "||"
-                },
-                {
-                    "domain": ".coursesu.com",
-                    "expirationDate": 1758198786.734987,
-                    "hostOnly": "false",
-                    "httpOnly": "false",
-                    "name": "tfpsi",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "ab68e120-d34e-41a9-a050-1bfe8618d6cb"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "expirationDate": 1789732986,
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "_lm_id",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "HS8Y3IFNLKA1H691"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "expirationDate": 1758198785.319777,
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "availableDeliveryMethods",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "drive%2Cdelivery%2Cdelivery%2Cdelivery"
-                },
-                {
-                    "domain": ".coursesu.com",
-                    "expirationDate": 1789733069.780824,
-                    "hostOnly": "false",
-                    "httpOnly": "false",
-                    "name": "datadome",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "ALkxeBwMbKusWWNEW684SA83K9gRx16NKK~ki2VSp7ZV3_XLdldKv445dwXJyumq0Xl3itGPwIE~d44eHUxafNxFeygZ53tMsf34gMn~ER2W6RMofsNF1FyFwJc37gL7"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "__cq_dnt",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "true",
-                    "storeId": "null",
-                    "value": "0"
-                },
-                {
-                    "domain": ".coursesu.com",
-                    "expirationDate": 1792360974,
-                    "hostOnly": "false",
-                    "httpOnly": "false",
-                    "name": "_cs_c",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "1"
-                },
-                {
-                    "domain": ".coursesu.com",
-                    "hostOnly": "false",
-                    "httpOnly": "false",
-                    "name": "_cs_cvars",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "true",
-                    "storeId": "null",
-                    "value": "%7B%7D"
-                },
-                {
-                    "domain": ".coursesu.com",
-                    "expirationDate": 1792283309,
-                    "hostOnly": "false",
-                    "httpOnly": "false",
-                    "name": "_cs_id",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "045a620a-28f0-abba-8b1a-4b78c3468458.1758119309.2.1758196987.1758196874.1739955371.1792283309787.1.x"
-                },
-                {
-                    "domain": ".coursesu.com",
-                    "expirationDate": 1758198865,
-                    "hostOnly": "false",
-                    "httpOnly": "false",
-                    "name": "_cs_s",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "3.5.U.9.1758198865897"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "expirationDate": 1758197586.912049,
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "checkReservationSlots",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "2025-09-18T12%3A11%3A06.881Z"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "cqcid",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "true",
-                    "storeId": "null",
-                    "value": "bc1iRcJmjanjIXTohieu76NANa"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "dw_dnt",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "true",
-                    "storeId": "null",
-                    "value": "0"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "dwac_8d8721aecf7af8fe498c1f352b",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "true",
-                    "storeId": "null",
-                    "value": "HUZDBwW74JFrBPwJQnHsNlFEBFx_ksSg4GY%3D|dw-only|||EUR|false|Europe%2FParis|true"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "expirationDate": 1773748970.193778,
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "dwanonymous_8ce4ace52d14ef4ce78780bbb2b979c7",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "bc1iRcJmjanjIXTohieu76NANa"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "sid",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "true",
-                    "storeId": "null",
-                    "value": "HUZDBwW74JFrBPwJQnHsNlFEBFx_ksSg4GY"
-                },
-                {
-                    "domain": "www.coursesu.com",
-                    "expirationDate": 1765972984.498117,
-                    "hostOnly": "true",
-                    "httpOnly": "false",
-                    "name": "storeId",
-                    "path": "/",
-                    "sameSite": "no_restriction",
-                    "secure": "true",
-                    "session": "false",
-                    "storeId": "null",
-                    "value": "37005"
-                }
-            ]
+    connector: ConnectorNodriver = ConnectorNodriver()
 
     async def start(self, query: str, store_id: str):
         # query: str = getattr(self, "query", None)
@@ -288,11 +37,10 @@ class SuperUProductsSpider:
         if query is None:
             raise AttributeError("Missing 'query' argument")
         
-        url: str = "https://www.coursesu.com"
-        connector: ConnectorNodriver = ConnectorNodriver()
-        await connector.get_page(f"{url}?q={quote_plus(query)}")
+        url: str = f"https://www.coursesu.com/recherche?q={quote_plus(query).replace(' ', '+')}"
+        await self.connector.get_page(url)
        
 
-    def parse(self, response, **kwargs):
+    def parse(self, response):
         # 'response' contains the page as seen by the browser
-        return {"url": response.url}
+       pass
