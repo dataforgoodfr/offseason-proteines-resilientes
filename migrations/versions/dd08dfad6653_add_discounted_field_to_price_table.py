5dc8eef2ab02e625344f41779c865c93f5a71dc9
choogland <christine.hoogland@finddx.org>
2025-10-06 22:09:49 +0200

    Add 'discounted' field to 'Price' table (#50)
    * Add 'discounted' field to 'Price' table
    
    - Add 'discounted' (bool) field to 'Price' table
    - Add migration script
    - Update mermaid diagram & SVG diagram

--- migrations/versions/dd08dfad6653_add_discounted_field_to_price_table.py
+++ migrations/versions/dd08dfad6653_add_discounted_field_to_price_table.py
@@ -1,8 +1,8 @@
-"""Add 'disabled' field to 'Product' table
+"""Add 'discounted' field to 'Price' table
 
-Revision ID: 74f8c9658d25
-Revises:
-Create Date: 2025-08-11 13:56:23.604198
+Revision ID: dd08dfad6653
+Revises: d483c555d6d1
+Create Date: 2025-09-30 12:03:23.308390
 
 """
 
@@ -12,8 +12,8 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = "74f8c9658d25"
-down_revision: Union[str, Sequence[str], None] = None
+revision: str = "dd08dfad6653"
+down_revision: Union[str, Sequence[str], None] = "d483c555d6d1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -22,11 +22,11 @@ def upgrade() -> None:
     """Upgrade schema."""
 
     op.add_column(
-        "product", sa.Column("disabled", sa.BOOLEAN, server_default=sa.sql.false())
+        "price", sa.Column("discounted", sa.BOOLEAN, server_default=sa.sql.false())
     )
 
 
 def downgrade() -> None:
     """Downgrade schema."""
 
-    op.drop_column("product", "disabled")
+    op.drop_column("price", "discounted")
--- src/models/price.py
+++ src/models/price.py
@@ -1,9 +1,11 @@
+from typing import Optional
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey, String
+from sqlalchemy import ForeignKey, String, Boolean
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import false
 from sqlalchemy.types import Numeric
 
 from .base import Base
@@ -24,6 +26,7 @@ class Price(Base):
 
     amount: Mapped[float] = mapped_column(Numeric(4, 2))
     currency: Mapped[str] = mapped_column(String(3), default="EUR")
+    discounted: Mapped[Optional[bool]] = mapped_column(Boolean, server_default=false())
 
     source_id: Mapped[int] = mapped_column(ForeignKey("source.id"))
     source: Mapped["Source"] = relationship(back_populates="price")
