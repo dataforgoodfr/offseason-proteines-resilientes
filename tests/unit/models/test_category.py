e8cc9c5abde47fbfa898ab1d9e0739d574c99d0e
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2025-12-05 01:14:11 +0100

    Fix unit tests
    Commit 3ae2e16 has updated the normalised categories without updating
    the related unit tests.
    
    This commit fixes the unit tests.

--- tests/unit/models/test_category.py
+++ tests/unit/models/test_category.py
@@ -15,19 +15,19 @@ class TestCategory(unittest.TestCase):
     def test_string_to_enum(self):
         category = CategoryValues("Viandes")
         self.assertIsInstance(category, CategoryValues)
-        self.assertIs(category, CategoryValues.VIANDE)
+        self.assertIs(category, CategoryValues.VIANDES)
 
         category = CategoryValues("Poissons et fruits de mer")
         self.assertIsInstance(category, CategoryValues)
-        self.assertIs(category, CategoryValues.POISSON)
+        self.assertIs(category, CategoryValues.POISSONS)
 
         category = CategoryValues("Œufs et produits laitiers")
         self.assertIsInstance(category, CategoryValues)
-        self.assertIs(category, CategoryValues.LAITIER)
+        self.assertIs(category, CategoryValues.OEUFS_PRODUITS_LAITIERS)
 
         category = CategoryValues("Légumineuses")
         self.assertIsInstance(category, CategoryValues)
-        self.assertIs(category, CategoryValues.LEGUMINEUSE)
+        self.assertIs(category, CategoryValues.LEGUMINEUSES)
 
         category = CategoryValues("Produits à base de soja")
         self.assertIsInstance(category, CategoryValues)
@@ -35,11 +35,11 @@ class TestCategory(unittest.TestCase):
 
         category = CategoryValues("Légumes et assimilés")
         self.assertIsInstance(category, CategoryValues)
-        self.assertIs(category, CategoryValues.LEGUME)
+        self.assertIs(category, CategoryValues.LEGUMES)
 
         category = CategoryValues("Céréales et pseudo-céréales")
         self.assertIsInstance(category, CategoryValues)
-        self.assertIs(category, CategoryValues.CEREALE)
+        self.assertIs(category, CategoryValues.CEREALES)
 
         category = CategoryValues("Noix et graines")
         self.assertIsInstance(category, CategoryValues)
@@ -47,11 +47,11 @@ class TestCategory(unittest.TestCase):
 
         category = CategoryValues("Poudres protéinées")
         self.assertIsInstance(category, CategoryValues)
-        self.assertIs(category, CategoryValues.POUDRE)
+        self.assertIs(category, CategoryValues.POUDRES)
 
         category = CategoryValues("Alternatives végétales")
         self.assertIsInstance(category, CategoryValues)
-        self.assertIs(category, CategoryValues.ALTERNATIVE)
+        self.assertIs(category, CategoryValues.ALTERNATIVES)
 
         with self.assertRaises(ValueError):
             category = CategoryValues("XYZ")
