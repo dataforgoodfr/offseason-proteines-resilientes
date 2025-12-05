68fb439e85d24036e2f239b8409d46c774dc8c37
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2025-12-05 09:11:06 +0100

    Move 'set_up_root_logger' into shared util module

---
+++ src/utils/logger.py
@@ -0,0 +1,19 @@
+from logging import INFO, Formatter, Logger, StreamHandler, getLogger
+
+
+def set_up_root_logger(level=INFO) -> Logger:
+    """
+    Sets up the root logger.
+    """
+
+    root_logger = getLogger("")
+    root_logger.setLevel(level)
+
+    console = StreamHandler()
+
+    console_formatter = Formatter("%(name)-12s: %(levelname)-8s %(message)s")
+    console.setFormatter(console_formatter)
+
+    root_logger.addHandler(console)
+
+    return root_logger
