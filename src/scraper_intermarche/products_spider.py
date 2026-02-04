92bceb4829cbe681d2ec4e64acb010e790324f52
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2026-02-04 14:09:14 +0100

    Fix a typo in a comment

--- src/scraper_intermarche/products_spider.py
+++ src/scraper_intermarche/products_spider.py
@@ -259,7 +259,7 @@ class IntermarcheProductsSpider(Spider, ProductSpider):
         response,
     ) -> tuple[bool, float, float | None] | None:
         """
-        Extracts wether or not the product is discounted and its both prices
+        Extracts whether or not the product is discounted and its both prices
         (normal and discounted) from the response.
 
         Returns None if no price can be found.
