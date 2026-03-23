dataforgoodfr/offseason-proteines-resilientes

commit 62018ffdc3251d7ee2240402d8f06b1b39ab0397

Handle HTTP errors in OFFDC

src/off_data_collector/cli.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from openfoodfacts import API as off_api
 from openfoodfacts.types import JSONType
+from requests.exceptions import HTTPError, ReadTimeout
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
@@ -161,7 +162,14 @@ def main() -> None:
                     logger.debug(f"Product with reference ID '{reference}' is disabled")
                     continue
 
-                data = __fetch_product_data(api_client, reference)
+                try:
+                    data = __fetch_product_data(api_client, reference)
+                except (HTTPError, ReadTimeout) as err:
+                    logger.error(
+                        f"Reference {reference} skipped due to an error: {err}"
+                    )
+                finally:
+                    continue
 
                 if data is None:
                     continue

commit 5bff4d0e8f12e20144a3448dc4d0977ede68fe58

Fix OFF data insertion
Commit 62018ffd introduced a silly mistake of adding a 'finally' statement
preventing data from being updated.

This commit fixes the issue and improving the overall error handling.

Co-authored-by: Paul-Emmanuel Raoul <skyper@skyplabs.net>

src/off_data_collector/cli.py
@@ -6,7 +6,6 @@ from pathlib import Path
 from openfoodfacts import API as off_api
 from openfoodfacts.types import JSONType
 from pyrate_limiter import Duration, limiter_factory
-from requests.exceptions import HTTPError, ReadTimeout
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
@@ -146,15 +145,23 @@ async def __run() -> None:
 
             # If the product does not already exist in the database.
             if products.count() == 0:
-                data = await __fetch_product_data(api_client, reference)
+                try:
+                    data = await __fetch_product_data(api_client, reference)
+                except Exception as err:
+                    logger.error(
+                        f"Reference {reference} skipped due to an error when fetching the data: {err}"
+                    )
+                    continue
 
                 if data is None:
                     continue
 
                 try:
                     new_product = __create_new_product(reference, data)
-                except InvalidUnitError as err:
-                    logger.error(err)
+                except Exception as err:
+                    logger.error(
+                        f"Reference {reference} skipped due to an error when processing the data: {err}"
+                    )
                     continue
 
                 session.add(new_product)
@@ -176,11 +183,10 @@ async def __run() -> None:
 
                 try:
                     data = await __fetch_product_data(api_client, reference)
-                except (HTTPError, ReadTimeout) as err:
+                except Exception as err:
                     logger.error(
-                        f"Reference {reference} skipped due to an error: {err}"
+                        f"Reference {reference} skipped due to an error when fetching the data: {err}"
                     )
-                finally:
                     continue
 
                 if data is None: